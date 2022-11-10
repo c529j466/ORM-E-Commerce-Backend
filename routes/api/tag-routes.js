292d1d29c4102d3c54de04d91f1d148a48c5349f
Christian Jones <c529j466@yahoo.com>
2022-11-10 00:47:22 -0600

    route to find all tags and its products

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,9 +3,17 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    //  findAll tags
+    const tagData = await Tag.findAll({
+      // Include its Products
+      include: [{ model: Product, through: ProductTag, as: "tagged_products" }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
