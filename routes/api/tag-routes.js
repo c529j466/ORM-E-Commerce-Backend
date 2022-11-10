4b805eb5e92cb6fddb43d531dd512ac013cf2da8
Christian Jones <c529j466@yahoo.com>
2022-11-10 01:05:32 -0600

    route to create tag finished

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,8 +37,13 @@ router.get('/:id', async (req, res) => {
 });
 
   // UPDATE route to create a new tag
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // UPDATE route for a tag's name by its `id` value
